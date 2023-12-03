aad82d080e64ef0c9a08ac6ffbe22e99e3cee81a
Saikat Das <saikatdas40g@gmail.com>
2023-12-03 15:21:17 +0530

    started backend for main app

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+  path: './env'
+})
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 9000, () => {
+    console.log('app is listening');
+  });
+  app.get('/', (req, res) => {
+    res.send("Hii from computer");
+  })
+})
+.catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+/*
+import { express } from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log("APp is listening");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
+*/
