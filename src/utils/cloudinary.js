8c5da36f9731ee15b49b6f5c17ca74b0003bbc51
Saikat Das <saikatdas40g@gmail.com>
2023-12-06 17:08:23 +0530

    added base

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,6 +2,7 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -9,18 +10,19 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (fileLocationPath) => {
+const uploadOnCloudinary = async (fileLocationPath) => { 
   try {
-    
+    if(!fileLocationPath) return null;
+    cloudinary.uploader.upload(fileLocationPath, {
+      resource_type: "auto"
+    })
+    //file has been uploaded successfully
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
   } catch (error) {
-    
+    fs.unlinkSync(fileLocationPath) //remove locally saved temporary file as upload opearation failed
+    return null;
   }
-}
+};
 
-cloudinary.v2.uploader.upload(
-  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) {
-    console.log(result);
-  }
-);
+export {uploadOnCloudinary}
